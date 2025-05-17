4df079069489197e4e5c5a14edd48a2389f83136
Vasile Macovei <vasilache071@gmail.com>
2025-05-17 17:43:33 +0100

    Fin de la calculadora que permite operaciones matematicas basicas con 2 operandos

--- src/main/java/daw/PanelPrincipal.java
+++ src/main/java/daw/PanelPrincipal.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
@@ -50,7 +49,7 @@ public class PanelPrincipal extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent ae) {
         // Se obtiene el objeto que desencadena el evento
         Object o = ae.getSource();
-        
+
         List<String> listaOperacionesDisponibles = new ArrayList<>();
         listaOperacionesDisponibles.add("+");
         listaOperacionesDisponibles.add("-");
@@ -58,38 +57,52 @@ public class PanelPrincipal extends JPanel implements ActionListener {
         listaOperacionesDisponibles.add("/");
         listaOperacionesDisponibles.add("C");
         listaOperacionesDisponibles.add("=");
-        
+
         String textoButon = ((JButton) o).getText();
         if (textoButon.equals("C")) {
             operacionIntroducida = "";
-        } else if (textoButon.equals("=")){
+        } else if (textoButon.equals("=")) {
+            // Muestro el resultado de la operacion introducida por el usuario
             areaTexto.setText(calcularResultadoOperacion(operacionIntroducida));
-        } else if (listaOperacionesDisponibles.contains(textoButon)){
+        } else if (listaOperacionesDisponibles.contains(textoButon)) {
+            // Añado espacios antes y despues del tipo de operacion para poder
+            // hacer despues correctamente el split
             operacionIntroducida += " " + textoButon + " ";
         } else {
             operacionIntroducida += textoButon;
         }
         
-        if (!textoButon.contains("=")){
+        // En caso de no ser el caracter "=" muestro la operacion que ha
+        // introducido hasta el momento el usuario
+        if (!textoButon.contains("=")) {
             areaTexto.setText(operacionIntroducida);
         }
     }
-    
-    public String calcularResultadoOperacion(String operacionIntroducida){
-        String[] operacionUsuario = operacionIntroducida.split(" ");
+
+    public String calcularResultadoOperacion(String operacionIntroducida) {
+        String[] operacionUsuario = operacionIntroducida.trim().split(" ");
+        // System.out.println(Arrays.toString(operacionUsuario));
         double resultado = 0.0;
-        double numero1 = Double.parseDouble(operacionUsuario[0]);
-        double numero2 = Double.parseDouble(operacionUsuario[2]);
-        if (operacionIntroducida.contains("*")) {
-            resultado = numero1 * numero2;
-        } else if (operacionIntroducida.contains("+")) {
-            resultado = numero1 + numero2;
-        } else if (operacionIntroducida.contains("/")) {
-            resultado = numero1 / numero2;
-        } else if (operacionIntroducida.contains("-")) {
-            resultado = numero1 - numero2;
+        // Si hay dos operandos hago el calculo correspondiente
+        if (operacionUsuario.length == 3) {
+            double numero1 = Double.parseDouble(operacionUsuario[0]);
+            double numero2 = Double.parseDouble(operacionUsuario[2]);
+            if (operacionIntroducida.contains("*")) {
+                resultado = numero1 * numero2;
+            } else if (operacionIntroducida.contains("+")) {
+                resultado = numero1 + numero2;
+            } else if (operacionIntroducida.contains("/")) {
+                resultado = numero1 / numero2;
+            } else if (operacionIntroducida.contains("-")) {
+                resultado = numero1 - numero2;
+            }
+            System.out.println(resultado);
+            // En caso contrario muestro un mensaje de error
+        } else {
+            JOptionPane.showMessageDialog(null, "No has introducido dos"
+                    + " operandos, o has introducido más de 2 operandos y una"
+                    + " operación");
         }
-        System.out.println(resultado);
         return resultado + "";
     }
 }
